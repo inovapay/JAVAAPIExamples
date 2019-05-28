aff29fc63906c631cb3559974c9027bf68ff3687
Diego Convers <diego.convers@firefly-e.com>
2019-05-28 11:39:10 -0500

    Updates on API services. Api/status parameters and payload update

--- src/main/java/com/inovapay/inovapin/javaapisamples/UATInovapayGatewayTranStatus.java
+++ src/main/java/com/inovapay/inovapin/javaapisamples/UATInovapayGatewayTranStatus.java
@@ -7,6 +7,7 @@ package com.inovapay.inovapin.javaapisamples;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -15,7 +16,7 @@ import org.json.simple.parser.JSONParser;
  * @author dclav
  */
 public class UATInovapayGatewayTranStatus {
-    
+
     private static final Logger LOGGER = Logger.getLogger(UATInovapayGatewayTranStatus.class.getName());
 
     public static void main(String args[]) {
@@ -28,10 +29,22 @@ public class UATInovapayGatewayTranStatus {
     }
 
     public static JSONObject process() throws Exception {
+        JSONArray references = new JSONArray(); // Reference of past transaction given by Merchant, list is to be provided in case multiple queries
+        JSONArray tranDates = new JSONArray(); // Transaction date. Can be single date to inquire a particular date, or two values to specify from and to date (range)
+
         JSONObject payload = new JSONObject();
 
-        payload.put("reference", "522125"); // Reference of past transaction given by Merchant
-        
+        references.add("522125");
+        references.add("522126");
+
+        tranDates.add("2019-05-11"); // Dates on yyyy-MM-dd format.
+        tranDates.add("2019-05-24"); // Dates on yyyy-MM-dd format.
+
+        payload.put("status", "Canceled");// Transaction status to search (values: "Pending", "Completed", "Canceled")
+
+        payload.put("reference", references);
+        payload.put("date", tranDates);
+
         InovapayRequest inovapayRequest = new InovapayRequest();
         String path = "api/status";
         String response = inovapayRequest.doRequest("POST", path, payload);
@@ -45,5 +58,5 @@ public class UATInovapayGatewayTranStatus {
 
         return result;
     }
-    
+
 }
